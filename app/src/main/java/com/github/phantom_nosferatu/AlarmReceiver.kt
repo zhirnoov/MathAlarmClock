378de81c1e62166d0434a0e3d7cb398d8800daae
Admin <phantom.nosferatu13@gmail.com>
2022-07-23 11:29:13 +0300

    create broadcast receiver

---
+++ app/src/main/java/com/github/phantom_nosferatu/AlarmReceiver.kt
@@ -0,0 +1,14 @@
+package com.github.phantom_nosferatu
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import java.util.*
+
+class AlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+        Log.d("MainActivity", "Alarm Receiver ${Date()}")
+    }
+
+}
--- app/src/main/java/com/github/phantom_nosferatu/MainActivity.kt
+++ app/src/main/java/com/github/phantom_nosferatu/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.github.phantom_nosferatu
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+lateinit var alarmManager : AlarmManager
+lateinit var pendingIntent : PendingIntent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+       alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+
     }
 }
