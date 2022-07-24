c314b3a060b0143322186949de35071e71f46d3f
Admin <phantom.nosferatu13@gmail.com>
2022-07-24 10:50:17 +0300

    try simple alarm manager call

--- app/src/main/java/com/github/phantom_nosferatu/MainActivity.kt
+++ app/src/main/java/com/github/phantom_nosferatu/MainActivity.kt
@@ -4,18 +4,26 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
-lateinit var alarmManager : AlarmManager
-lateinit var pendingIntent : PendingIntent
+lateinit var alarmManager: AlarmManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val receiver = Intent(this, AlarmReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(this, 0, receiver, PendingIntent.FLAG_UPDATE_CURRENT)
+        val alarmButton = findViewById<Button>(R.id.btn_alarm)
 
-
+        alarmButton.setOnClickListener {
+            Log.d("MainActivity", "Create ${Date()}")
+            alarmManager.setAlarmClock(AlarmManager.AlarmClockInfo(System.currentTimeMillis() + 10000, pendingIntent), pendingIntent)
+        }
     }
 }
