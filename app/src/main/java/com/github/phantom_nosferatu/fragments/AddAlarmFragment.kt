878de602fbe84cb3cff029c01f2c858db70dbf1b
Admin <phantom.nosferatu13@gmail.com>
2022-07-25 21:03:37 +0300

    set nav host in activity xml

--- app/src/main/java/com/github/phantom_nosferatu/fragments/AddAlarmFragment.kt
+++ app/src/main/java/com/github/phantom_nosferatu/fragments/AddAlarmFragment.kt
@@ -4,16 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TimePicker
 import androidx.fragment.app.Fragment
 import com.github.phantom_nosferatu.R
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 
 class AddAlarmFragment : Fragment() {
+
+    private lateinit var timePicker : MaterialTimePicker
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_add_alarm, container, false)
+        val view = inflater.inflate(R.layout.fragment_add_alarm, container, false)
+        timePicker = view.findViewById(R.id.time_picker)
+        timePicker = MaterialTimePicker.Builder()
+            .setTimeFormat(TimeFormat.CLOCK_24H)
+            .setHour(12)
+            .setMinute(0)
+            .setTitleText("Выберите текст")
+            .build()
+
+        return view
     }
 }
