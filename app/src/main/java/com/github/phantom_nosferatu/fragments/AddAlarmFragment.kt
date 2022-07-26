513676e9efe8f5009bdfe464e43b66db0e523681
Admin <phantom.nosferatu13@gmail.com>
2022-07-26 15:59:01 +0300

    add material time picker

--- app/src/main/java/com/github/phantom_nosferatu/fragments/AddAlarmFragment.kt
+++ app/src/main/java/com/github/phantom_nosferatu/fragments/AddAlarmFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TimePicker
 import androidx.fragment.app.Fragment
 import com.github.phantom_nosferatu.R
@@ -12,18 +13,31 @@ import com.google.android.material.timepicker.TimeFormat
 
 class AddAlarmFragment : Fragment() {
 
+    private lateinit var timePickerButton : Button
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_add_alarm, container, false)
-        val timePicker = MaterialTimePicker.Builder()
-            .setTimeFormat(TimeFormat.CLOCK_24H)
-            .setHour(12)
-            .setMinute(0)
-            .setTitleText("Выберите текст")
-            .build()
+        timePickerButton = view.findViewById(R.id.btn_timepicker)
+        timePickerButton.setOnClickListener {
+            val timePicker = MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_24H)
+                .setHour(12)
+                .setMinute(0)
+                .setTitleText("Выберите текст")
+                .build()
+
+            timePicker.show(childFragmentManager, "timepicker")
+
+            timePicker.addOnPositiveButtonClickListener {
+                val hour = timePicker.hour.toString().padStart(2, '0')
+                val minute = timePicker.minute.toString().padStart(2, '0')
+                timePickerButton.text = "$hour:$minute"
+            }
+        }
         return view
     }
 }
--- app/src/main/java/com/github/phantom_nosferatu/fragments/AllAlarmsFragment.kt
+++ app/src/main/java/com/github/phantom_nosferatu/fragments/AllAlarmsFragment.kt
@@ -5,15 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.github.phantom_nosferatu.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class AllAlarmsFragment : Fragment() {
+
+    private lateinit var addButton : FloatingActionButton
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_all_alarms, container, false)
+        val view = inflater.inflate(R.layout.fragment_all_alarms, container, false)
+        addButton = view.findViewById(R.id.btn_add)
+        addButton.setOnClickListener{
+            Navigation.findNavController(view).navigate(R.id.action_allAlarmsFragment_to_addAlarmFragment)
+        }
+        return view
     }
 }
