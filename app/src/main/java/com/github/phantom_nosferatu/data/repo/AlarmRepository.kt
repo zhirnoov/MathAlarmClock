ab5c7a63600d15ba3bb7bd832d5ec59eca79ddb8
Admin <phantom.nosferatu13@gmail.com>
2022-08-06 16:53:41 +0300

    create repository

--- app/src/main/java/com/github/phantom_nosferatu/data/repo/AlarmRepository.kt
+++ app/src/main/java/com/github/phantom_nosferatu/data/repo/AlarmRepository.kt
@@ -1,4 +1,15 @@
 package com.github.phantom_nosferatu.data.repo
 
-class AlarmRepository {
+import com.github.phantom_nosferatu.data.dao.AlarmDao
+import com.github.phantom_nosferatu.data.model.Alarm
+
+class AlarmRepository(private val alarmDao: AlarmDao) {
+
+    suspend fun saveAlarm(alarm: List<Alarm>) {
+        alarmDao.saveAlarm(alarm)
+    }
+
+    suspend fun getAlarms(): List<Alarm> {
+        return alarmDao.getAlarms()
+    }
 }
