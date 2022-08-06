fe3bcd98e9f1f88b0f3489460afef5c86e63b2d1
Admin <phantom.nosferatu13@gmail.com>
2022-08-06 16:58:13 +0300

    create addAlarmViewModel

---
+++ app/src/main/java/com/github/phantom_nosferatu/view_models/AddAlarmViewModel.kt
@@ -0,0 +1,16 @@
+package com.github.phantom_nosferatu.view_models
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.github.phantom_nosferatu.data.model.Alarm
+import com.github.phantom_nosferatu.data.repo.AlarmRepository
+import kotlinx.coroutines.launch
+
+class AddAlarmViewModel(private val alarmRepository: AlarmRepository) : ViewModel() {
+
+    fun saveAlarm(alarm : List<Alarm>) {
+     viewModelScope.launch {
+         alarmRepository.saveAlarm(alarm)
+     }
+    }
+}
