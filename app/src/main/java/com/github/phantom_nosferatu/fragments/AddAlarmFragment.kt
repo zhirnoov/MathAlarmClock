9e3327fe420d4c3e76ee17830829d667314bcbcf
unknown <phantom.nosferatu13@gmail.com>
2022-08-11 20:10:45 +0300

    format time

--- app/src/main/java/com/github/phantom_nosferatu/fragments/AddAlarmFragment.kt
+++ app/src/main/java/com/github/phantom_nosferatu/fragments/AddAlarmFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.NumberPicker
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
@@ -24,13 +25,14 @@ import com.google.android.material.timepicker.TimeFormat
 
 class AddAlarmFragment : Fragment() {
 
-    private lateinit var timePickerButton: Button
+    private lateinit var hourPicker: NumberPicker
+    private lateinit var minutePicker: NumberPicker
     private lateinit var saveButton: Button
     private val alarmHelper = AlarmHelper()
-    private val viewmodel: AddAlarmViewModel by viewModels {
+    private val viewModel: AddAlarmViewModel by viewModels {
         AddAlarmViewModelFactory(App().alarmRepository)
     }
-    private var hour: Int = 5
+    private var hour: Int = 0
     private var minute: Int = 0
 
     @RequiresApi(Build.VERSION_CODES.N)
@@ -40,49 +42,51 @@ class AddAlarmFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_add_alarm, container, false)
-        timePickerButton = view.findViewById(R.id.btn_timepicker)
+        hourPicker = view.findViewById(R.id.picker_hour)
+        minutePicker = view.findViewById(R.id.picker_minute)
         saveButton = view.findViewById(R.id.btn_save)
+
+        hourPicker.minValue = 0
+        hourPicker.maxValue = 23
+        hourPicker.setFormatter {
+            String.format("%02d", it)
+        }
+        hourPicker.wrapSelectorWheel = true
+
+        minutePicker.minValue = 0
+        minutePicker.maxValue = 59
+        minutePicker.setFormatter {
+            String.format("%02d", it)
+        }
+
+
+        hourPicker.setOnValueChangedListener { numberPicker, i, i2 ->
+            hour = hourPicker.value
+        }
+
+        minutePicker.setOnValueChangedListener { numberPicker, i, i2 ->
+            minute = minutePicker.value
+        }
 
         if (savedInstanceState != null) {
             hour = savedInstanceState.getInt("INT_HOUR", hour)
             minute = savedInstanceState.getInt("INT_MINUTE", minute)
-            timePickerButton.text =
-                "${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}"
 
         }
-        //Property hour should be initialized before get
 
-        timePickerButton.setOnClickListener {
-            val timePicker = MaterialTimePicker.Builder()
-                .setTimeFormat(TimeFormat.CLOCK_24H)
-                .setHour(12)
-                .setMinute(0)
-                .setTitleText("Выберите текст")
-                .build()
-
-            timePicker.show(childFragmentManager, "timepicker")
-
-            timePicker.addOnPositiveButtonClickListener {
-                hour = timePicker.hour
-                minute = timePicker.minute
-                timePickerButton.text =
-                    "${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}"
-            }
-
-            saveButton.setOnClickListener {
-                alarmHelper.setAlarm(requireContext(), hour, minute)
-                viewmodel.saveAlarm(
-                    Alarm(
-                        id = 0,
-                        title = "тест",
-                        hour = hour,
-                        minute = minute,
-                        isActive = true
-                    )
+        saveButton.setOnClickListener {
+            alarmHelper.setAlarm(requireContext(), hour, minute)
+            viewModel.saveAlarm(
+                Alarm(
+                    id = 0,
+                    title = "тест",
+                    hour = hour,
+                    minute = minute,
+                    isActive = true
                 )
-                Toast.makeText(context, "Alarm $hour:$minute create", Toast.LENGTH_SHORT).show()
-                findNavController().popBackStack()
-            }
+            )
+            Toast.makeText(context, "Alarm $hour:$minute create", Toast.LENGTH_SHORT).show()
+            findNavController().popBackStack()
         }
         return view
     }
@@ -92,5 +96,4 @@ class AddAlarmFragment : Fragment() {
         outState.putInt("INT_HOUR", hour)
         outState.putInt("INT_MINUTE", minute)
     }
-
 }
