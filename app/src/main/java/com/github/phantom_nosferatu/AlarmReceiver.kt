00f4a91aa6c44e08efd8ac6ff79af7d9ed8ab8cb
unknown <phantom.nosferatu13@gmail.com>
2022-08-12 20:32:39 +0300

    add alarm sound

--- app/src/main/java/com/github/phantom_nosferatu/AlarmReceiver.kt
+++ app/src/main/java/com/github/phantom_nosferatu/AlarmReceiver.kt
@@ -3,15 +3,19 @@ package com.github.phantom_nosferatu
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.media.MediaPlayer
+import android.media.RingtoneManager
 import android.util.Log
 import java.util.*
 
-val notificationHelper = NotificationHelper()
 class AlarmReceiver : BroadcastReceiver() {
+
     override fun onReceive(p0: Context, p1: Intent?) {
         Log.d("AlarmTesting", "Alarm Receiver ${Date()}")
+        val notificationHelper = NotificationHelper()
         notificationHelper.createNotificationChannel(p0)
         notificationHelper.createNofication(p0)
+        val mediaPlayer = MediaPlayer.create(p0, R.raw.alarm_sound)
+        mediaPlayer.start()
     }
-
 }
