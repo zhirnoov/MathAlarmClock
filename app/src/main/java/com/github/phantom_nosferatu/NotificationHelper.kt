6bc80b1d13f83277e9bc2ddb51e27757916cad64
unknown <phantom.nosferatu13@gmail.com>
2022-08-12 18:40:54 +0300

    add action to notification

--- app/src/main/java/com/github/phantom_nosferatu/NotificationHelper.kt
+++ app/src/main/java/com/github/phantom_nosferatu/NotificationHelper.kt
@@ -2,21 +2,32 @@ package com.github.phantom_nosferatu
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
 class NotificationHelper {
-    val CHANNEL_ID = "101"
-    val notificationId = 101
+    private val CHANNEL_ID = "101"
+    private val notificationId = 101
 
     fun createNofication(context: Context) {
+        val actionIntent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val actionPendingIntent: PendingIntent =
+            PendingIntent.getActivity(context, 0, actionIntent, 0)
+
+
         val builder = NotificationCompat.Builder(context, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_baseline_add_24)
             .setContentTitle("Мой будильник")
             .setContentText("Будильник сработал")
             .setPriority(NotificationCompat.PRIORITY_MAX)
+            .addAction(R.drawable.ic_close_24, "Отключить", actionPendingIntent)
         with(NotificationManagerCompat.from(context)) {
             notify(notificationId, builder.build())
         }
