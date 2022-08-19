cc9bb4e5963a6be4a5f5228974f7466d9663c5c7
unknown <phantom.nosferatu13@gmail.com>
2022-08-19 17:54:29 +0300

    create solution fragment

--- app/src/main/java/com/github/phantom_nosferatu/fragments/AlarmSolutionFragment.kt
+++ app/src/main/java/com/github/phantom_nosferatu/fragments/AlarmSolutionFragment.kt
@@ -1,4 +1,30 @@
 package com.github.phantom_nosferatu.fragments
 
-class AlarmSolutionFragment : Fragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.github.phantom_nosferatu.R
+
+class AlarmSolutionFragment : Fragment() {
+
+    private lateinit var stopAudio : Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_alarm_solution, container, false)
+        stopAudio = view.findViewById(R.id.btn_stopAudio)
+
+        stopAudio.setOnClickListener {
+         TODO()
+        }
+
+        return view
+
+    }
 }
